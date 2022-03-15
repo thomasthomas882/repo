d2b75bdd956abac5caceb4592552d6119bbc6c11
thomasthomas882 <emilywyao123@gmail.com>
2022-03-14 20:20:09 -0400

    Added File

---
+++ addorsubtract.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class addorsubtract {
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.print("Enter first operand: ");
+
+        double firstoperand = scnr.nextDouble();
+
+        System.out.print("Enter second operand: ");
+
+        double secondoperand = scnr.nextDouble();
+
+        double result = firstoperand + secondoperand;
+
+        System.out.print("sum" + result);
+
+
+        double result2 = firstoperand - secondoperand;
+
+        System.out.print("difference" + result2);
+
+
+
+
+
+
+    }
+}
